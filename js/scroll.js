8d7280aad17113ed8d00ecdb0840146faf958cd8
PIGSx <hiury1315ajac@gmail.com>
2022-11-01 18:02:04 -0300

    adicionando o java do site e um scoll animado

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset : true})
+
+sr.reveal('.content', {duration: 1000});
+
+sr.reveal('.gallery', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 3000
+});
+
+sr.reveal('.service', {duration: 3000});
