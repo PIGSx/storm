329970c4381930599789c9864d6bf122d29d2f3e
PIGSx <hiury1315ajac@gmail.com>
2022-11-03 01:08:49 -0300

    adicionando scroll da pagina historia

---
+++ js/scrollH.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+  })
+  sr.reveal('.image',{ delay:350, origin:'left' })
+  sr.reveal('.about-text',{ delay:350, origin:'right' })
+  sr.reveal('.logoS-content',{ delay:350, origin:'left' })
+  sr.reveal('.logoS-image',{ delay:350, origin:'right' })
