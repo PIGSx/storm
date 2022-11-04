a4188ed3f6a9ce730e91d6375c9d7d578458ba4a
PIGSx <hiury1315ajac@gmail.com>
2022-11-03 21:58:35 -0300

    unificando todos os scroll num unico projeto

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,22 @@
-window.sr = ScrollReveal({reset : true})
-
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2000,
+    reset : true,
+  })
+/* pagina inicial */
 sr.reveal('.content', {duration: 1000});
-
+sr.reveal('.fundo img', {duration: 1000});
 sr.reveal('.gallery', {
     rotate: { x: 0, y: 80, z: 0},
-    duration: 3000
-});
+    duration: 2000});
+sr.reveal('.service', {duration: 2000});
 
-sr.reveal('.service', {duration: 3000});
+/* historia */
+sr.reveal('.image',{ delay:400, origin:'left' })
+sr.reveal('.about-text',{ delay:350, origin:'right' })
+sr.reveal('.logoS-content',{ delay:350, origin:'left' })
+sr.reveal('.logoS-image',{ delay:400, origin:'right' })
+
+/* quem somos*/
+sr.reveal('.row',{ delay:400, origin:'left' })
+sr.reveal('.home-img',{ delay:350, origin:'right' })
